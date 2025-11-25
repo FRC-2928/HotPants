7b0a831d402202d940b9e5495648b2a3226de9ff
= <=>
2025-11-24 20:01:14 -0800

    added swerveIO and SwerveIOCTRE

---
+++ src/main/java/frc/robot/subsystems/drive/SwerveIO.java
@@ -0,0 +1,70 @@
+package frc.robot.subsystems.drive;
+import com.ctre.phoenix6.swerve.SwerveDrivetrain;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import org.littletonrobotics.junction.AutoLog;
+
+public interface SwerveIO {
+    @AutoLog
+    class SwerveIOInputs {
+        public Pose2d Pose = new Pose2d();
+        public ChassisSpeeds Speeds = new ChassisSpeeds();
+        public SwerveModuleState[] ModuleStates;
+        public SwerveModuleState[] ModuleTargets;
+        public SwerveModulePosition[] ModulePositions;
+        public Rotation2d RawHeading = new Rotation2d();
+        public double Timestamp;
+        public double OdometryPeriod;
+        public int SuccessfulDaqs;
+        public int FailedDaqs;
+
+        void logState(SwerveDrivetrain.SwerveDriveState state) {
+            this.Pose = state.Pose;
+            this.RawHeading = state.RawHeading;
+            this.ModuleStates = state.ModuleStates;
+            this.ModuleTargets = state.ModuleTargets;
+            this.ModulePositions = state.ModulePositions;
+            this.Speeds = state.Speeds;
+            this.SuccessfulDaqs = state.SuccessfulDaqs;
+            this.FailedDaqs = state.FailedDaqs;
+            this.OdometryPeriod = state.OdometryPeriod;
+            this.Timestamp = state.OdometryPeriod;
+        }
+    }
+
+    @AutoLog
+    class ModuleIOInputs {
+        public double driveSupplyCurrentAmps = 0.0;
+        public double driveStatorCurrentAmps = 0.0;
+        public double driveAppliedVolts = 0.0;
+        public double driveTemperature = 0.0;
+
+        public double steerSupplyCurrentAmps = 0.0;
+        public double steerStatorCurrentAmps = 0.0;
+        public double steerAppliedVolts = 0.0;
+        public double steerTemperature = 0.0;
+    }
+
+    default void updateSwerveInputs(SwerveIOInputs inputs) {}
+
+    default void updateModuleInputs(ModuleIOInputs... inputs) {}
+
+    default void registerTelemetryFunction(SwerveIOInputs inputs) {}
+
+    default void setSwerveState(SwerveRequest request) {}
+
+    default void resetRotation() {}
+
+    default void resetToParamaterizedRotation(Rotation2d rotation2d) {}
+
+    default void updateSimState() {}
+
+    default void resetRobotTranslation(Translation2d translation2d) {}
+
+    default void refreshData() {}
+}
---
+++ src/main/java/frc/robot/subsystems/drive/SwerveIOCTRE.java
@@ -0,0 +1,130 @@
+package frc.robot.subsystems.drive;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.swerve.SwerveDrivetrain;
+import com.ctre.phoenix6.swerve.SwerveDrivetrainConstants;
+import com.ctre.phoenix6.swerve.SwerveModuleConstants;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SwerveIOCTRE extends SwerveDrivetrain implements SwerveIO {
+
+    HashMap<String, BaseStatusSignal> frontLeftSignals = new HashMap<>();
+    HashMap<String, BaseStatusSignal> frontRightSignals = new HashMap<>();
+    HashMap<String, BaseStatusSignal> backLeftSignals = new HashMap<>();
+    HashMap<String, BaseStatusSignal> backRightSignals = new HashMap<>();
+
+    Map<Integer, HashMap<String, BaseStatusSignal>> signalsMap = new HashMap<>();
+
+    public SwerveIOCTRE(
+            SwerveDrivetrainConstants constants,
+            SwerveModuleConstants<TalonFXConfiguration, TalonFXConfiguration, CANcoderConfiguration>...
+                    moduleConstants) {
+        super(TalonFX::new, TalonFX::new, CANcoder::new, constants, moduleConstants);
+        this.resetRotation(DriverStation.getAlliance().equals(DriverStation.Alliance.Blue) ? Rotation2d.kZero : Rotation2d.k180deg);
+
+        signalsMap.put(0, frontLeftSignals);
+        signalsMap.put(1, frontRightSignals);
+        signalsMap.put(2, backLeftSignals);
+        signalsMap.put(3, backRightSignals);
+
+        for (int i = 0; i < 4; i++) {
+            var driveMotor = this.getModule(i).getDriveMotor();
+            var steerMotor = this.getModule(i).getSteerMotor();
+
+            var moduleMap = signalsMap.get(i);
+
+            moduleMap.put("driveSupplyCurrentAmps", driveMotor.getSupplyCurrent());
+            moduleMap.put("driveStatorCurrentAmps", driveMotor.getStatorCurrent());
+            moduleMap.put("driveAppliedVolts", driveMotor.getMotorVoltage());
+            moduleMap.put("driveTemperature", driveMotor.getDeviceTemp());
+
+            moduleMap.put("steerSupplyCurrentAmps", steerMotor.getSupplyCurrent());
+            moduleMap.put("steerStatorCurrentAmps", steerMotor.getStatorCurrent());
+            moduleMap.put("steerAppliedVolts", steerMotor.getMotorVoltage());
+            moduleMap.put("steerTemperature", steerMotor.getDeviceTemp());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void registerTelemetryFunction(SwerveIOInputs inputs) {
+        this.registerTelemetry(state -> {
+            SwerveDriveState modifiedState = (SwerveDriveState) state;
+            modifiedState.Speeds = ChassisSpeeds.fromRobotRelativeSpeeds(
+                    ((SwerveDriveState) state).Speeds, ((SwerveDriveState) state).Pose.getRotation());
+            inputs.logState(modifiedState);
+        });
+    }
+
+    @Override
+    public void updateSwerveInputs(SwerveIOInputs inputs) {
+        var state = this.getStateCopy();
+        state.Speeds = ChassisSpeeds.fromRobotRelativeSpeeds(state.Speeds, state.Pose.getRotation());
+        inputs.logState(state);
+    }
+
+    @Override
+    public void setSwerveState(SwerveRequest request) {
+        this.setControl(request);
+    }
+
+    @Override
+    public void resetRotation() {
+        this.resetRotation(DriverStation.getAlliance().equals(DriverStation.Alliance.Blue) ? Rotation2d.kZero : Rotation2d.k180deg);
+    }
+
+    @Override
+    public void resetToParamaterizedRotation(Rotation2d rotation2d) {
+        this.resetRotation(rotation2d);
+    }
+
+    @Override
+    public void resetRobotTranslation(Translation2d translation2d) {
+        this.resetTranslation(translation2d);
+    }
+
+	//TODO: find out what this does
+    @Override
+    public void updateSimState() {
+        this.updateSimState(0.02, 13.00);
+    }
+
+    public void updateModuleInputs(ModuleIOInputs... inputs) {
+        for (int i = 0; i < 4; i++) {
+            var moduleMap = signalsMap.get(i);
+
+            inputs[i].driveSupplyCurrentAmps =
+                    moduleMap.get("driveSupplyCurrentAmps").getValueAsDouble();
+            inputs[i].driveStatorCurrentAmps =
+                    moduleMap.get("driveStatorCurrentAmps").getValueAsDouble();
+            inputs[i].driveAppliedVolts = moduleMap.get("driveAppliedVolts").getValueAsDouble();
+            inputs[i].driveTemperature = moduleMap.get("driveTemperature").getValueAsDouble();
+
+            inputs[i].steerSupplyCurrentAmps =
+                    moduleMap.get("steerSupplyCurrentAmps").getValueAsDouble();
+            inputs[i].steerStatorCurrentAmps =
+                    moduleMap.get("steerStatorCurrentAmps").getValueAsDouble();
+            inputs[i].steerAppliedVolts = moduleMap.get("steerAppliedVolts").getValueAsDouble();
+            inputs[i].steerTemperature = moduleMap.get("steerTemperature").getValueAsDouble();
+        }
+    }
+
+    @Override
+    public void refreshData() {
+        for (int i = 0; i < 4; i++) {
+            var moduleMap = signalsMap.get(i);
+            BaseStatusSignal.refreshAll(moduleMap.values().toArray(new BaseStatusSignal[] {}));
+        }
+    }
+}
